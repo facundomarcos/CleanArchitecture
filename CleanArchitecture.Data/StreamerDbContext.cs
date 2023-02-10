76660220fb70931a140985160d7e6e00ecfe4258
facundomarcos <facundomarcos@live.com.ar>
2023-02-10 19:21:02 -0300

    Add project files.

---
+++ CleanArchitecture.Data/StreamerDbContext.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Data
+{
+    public class StreamerDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=localhost\sqlexpress; 
+                       Initia Catalog=Streamer;Integrated Security=True");
+        }
+
+        public DbSet<Streamer>? Streamers { get; set; }
+
+        public DbSet<Video>? Videos { get; set; }
+
+
+    }
+}
+
+
+6 cpitulo
