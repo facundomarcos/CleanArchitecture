bb45a247dc59836436036832f77bd92cdf470c2f
facundomarcos <facundomarcos@live.com.ar>
2023-02-14 11:31:27 -0300

    cap 30

---
+++ CleanArchitecture.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Domain.Common;
+using System.Linq.Expressions;
+
+namespace CleanArchitecture.Application.Contracts.Persistence
+{
+    public interface IAsyncRepository<T> where T : BaseDomainModel
+    {
+        Task<IReadOnlyList<T>> GetAllAsync();
+
+        //Func<T> - IEnumerable - in memory - solo aceptan funciones
+        //Expression<Func<T>> - IQueryable - SQL Server - solo aceptan expression functions
+       //La implementacion es la misma Where(x => x.property == "value")
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T,bool>> predicate);
+    }
+}
