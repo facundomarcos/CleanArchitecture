cdc24213104e96040c6a38d5ccbcf297f8bf9925
facundomarcos <facundomarcos@live.com.ar>
2023-02-17 11:46:16 -0300

    cap 54

--- CleanArchitecture.Data/Migrations/20230217144350_CleanArchitectureFacu.Designer.cs
+++ CleanArchitecture.Data/Migrations/20230217144350_CleanArchitectureFacu.Designer.cs
@@ -4,6 +4,7 @@ using CleanArchitecture.Infraestructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CleanArchitecture.Data.Migrations
 {
     [DbContext(typeof(StreamerDbContext))]
-    partial class StreamerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230217144350_CleanArchitectureFacu")]
+    partial class CleanArchitectureFacu
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- CleanArchitecture.Data/Persistence/StreamerDbContext.cs
+++ CleanArchitecture.Data/Persistence/StreamerDbContext.cs
@@ -12,14 +12,14 @@ namespace CleanArchitecture.Infraestructure.Persistence
 
 
 
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    optionsBuilder.UseSqlServer(@"Data Source=localhost\sqlexpress; 
-        //               Initial Catalog=Streamer;Integrated Security=True")
-        //        .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name },
-        //                Microsoft.Extensions.Logging.LogLevel.Information)
-        //        .EnableSensitiveDataLogging();
-        //}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=localhost\sqlexpress; 
+                       Initial Catalog=Streamer;Integrated Security=True")
+                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name },
+                        Microsoft.Extensions.Logging.LogLevel.Information)
+                .EnableSensitiveDataLogging();
+        }
 
         public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
